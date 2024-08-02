f24fd1c795222ab5fe437dba34f8d0e50e475831
santoshPagire <santoshpagire1007@gmail.com>
2024-08-02 17:56:39 +0530

    files added

---
+++ order_proc/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+    user: 'user',
+    host: 'postgres-container',
+    database: 'mydatabase',
+    password: 'password',
+    port: 5432,
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello from Devops Training Assesment !');
+});
+
+app.get('/data', async (req, res) => {
+    const client = await pool.connect();
+    const result = await client.query('SELECT NOW()');
+    client.release();
+    res.send(result.rows);
+});
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
+
